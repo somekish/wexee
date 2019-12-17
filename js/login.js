d8b07edc456f0d08711ea709d392136ba8d8160b
Vicktor <somekish@gmail.com>
2019-12-18 00:35:31 +0300

    настройка анимации одним блоком

--- js/login.js
+++ js/login.js
@@ -1,50 +1,38 @@
 'use strict';
 
-var buttonReg = document.getElementById('buttonReg');
-var buttonAuth = document.getElementById('buttonAuth');
-var auth = document.getElementById('auth');
-var reg = document.getElementById('reg');
-var block1 = document.getElementById('block1');
-var block2 = document.getElementById('block2');
+let buttonReg = document.getElementById('buttonReg');
+let buttonAuth = document.getElementById('buttonAuth');
+let auth = document.getElementById('auth');
+let reg = document.getElementById('reg');
+let block1 = document.getElementById('block1');
 
-buttonReg.addEventListener('click', function(){
-    setTimeout(block1.classList.add('go-left'), 1000);
+buttonReg.addEventListener('click', function(){  
     auth.classList.toggle('hide');
-    reg.classList.toggle('hide');
     buttonReg.classList.toggle('hide');
-    buttonAuth.classList.toggle('hide');
-    setTimeout(block1.classList.toggle('hide'), 1000);
-    block2.classList.toggle('hide');
+    block1.classList.add('go-right');
+    setTimeout(function(){
+        reg.classList.toggle('hide');
+        buttonAuth.classList.toggle('hide');
+    }, 600);
+    block1.classList.remove('go-left');
 });
 
-buttonAuth.addEventListener('click', function(){
-    block2.classList.add('go-right');
-    auth.classList.toggle('hide');
+buttonAuth.addEventListener('click', function(){  
+    debugger
     reg.classList.toggle('hide');
-    buttonReg.classList.toggle('hide');
     buttonAuth.classList.toggle('hide');
-    setTimeout(block2.classList.toggle('hide'), 1000);
-    block1.classList.toggle('hide');
+    block1.classList.add('go-left');
+    setTimeout(function(){
+        auth.classList.toggle('hide');
+        buttonReg.classList.toggle('hide');
+    }, 750);
+    block1.classList.remove('go-right');
 });
 
 
-// buttonReg.addEventListener('click', function(){
-//     setTimeout(block1.classList.add('go-left'), 1000);
-//     auth.classList.toggle('hide');
-//     reg.classList.toggle('hide');
-//     buttonReg.classList.toggle('hide');
-//     buttonAuth.classList.toggle('hide');
-//     setTimeout(block1.classList.toggle('hide'), 1000);
-//     block2.classList.toggle('hide');
-    
-// });
 
-// buttonAuth.addEventListener('click', function(){
-//     block2.classList.add('go-right');
-//     auth.classList.toggle('hide');
-//     reg.classList.toggle('hide');
-//     buttonReg.classList.toggle('hide');
-//     buttonAuth.classList.toggle('hide');
-//     setTimeout(block2.classList.toggle('hide'), 1000);
-//     block1.classList.toggle('hide');
-// });
+
+
+
+
+
